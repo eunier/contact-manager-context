c2b6536d98ce2d02eb2df8e51c7596fe7dadfa56
Yunier Alvarez <yunieralvarezdev@icloud.com>
2019-03-13 10:12:41 -0400

    Add a context reduce for action

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,15 +5,18 @@ import { Consumer } from '../context';
 class Contact extends Component {
   state = { showContactInfo: false };
 
-  onDeleteClick = () => {};
+  onDeleteClick = (id, dispatch) => {
+    dispatch({ type: 'DELETE_CONTACT', payload: id });
+  };
 
   render() {
-    const { name, email, phone } = this.props.contact;
+    const { id, name, email, phone } = this.props.contact;
     const { showContactInfo } = this.state;
 
     return (
       <Consumer>
         {value => {
+          const { dispatch } = value;
           return (
             <div className="card card-body mb-3">
               <h4>
@@ -30,7 +33,7 @@ class Contact extends Component {
                 <i
                   className="fas fa-times"
                   style={{ cursor: 'pointer', float: 'right', color: 'red' }}
-                  onClick={this.onDeleteClick}
+                  onClick={this.onDeleteClick.bind(this, id, dispatch)}
                 />
               </h4>
               {showContactInfo ? (
