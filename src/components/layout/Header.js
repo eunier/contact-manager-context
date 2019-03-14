f11f100b732103797742025410aada41eca22c3e
Yunier Alvarez <yunieralvarezdev@icloud.com>
2019-03-14 13:12:09 -0400

    WIP - Add links, params and redirect

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
 
 const Header = props => {
   const { branding } = props;
@@ -13,9 +14,19 @@ const Header = props => {
         <div>
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <a href="/" className="nav-link">
+              <NavLink to="/" className="nav-link">
                 Home
-              </a>
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/contact/add" className="nav-link">
+                Add
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/about" className="nav-link">
+                About
+              </NavLink>
             </li>
           </ul>
         </div>
