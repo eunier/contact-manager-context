ed05202b5267a923b2a9297505626df6c8115ced
Yunier Alvarez <yunieralvarezdev@icloud.com>
2019-03-15 07:36:23 -0400

    WIP - GET requests

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,7 @@ export default () => {
   return (
     <div>
       <h1 className="display-4">About Contact Manager</h1>
-      <p classNames="lead">Simple app to manage contacts</p>
+      <p className="lead">Simple app to manage contacts</p>
       <p className="text-secondary">Version 1.0.0</p>
     </div>
   );
