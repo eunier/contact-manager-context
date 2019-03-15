a60d6c4f28a504ee207432a1f8ceec11e6b04840
Yunier Alvarez <yunieralvarezdev@icloud.com>
2019-03-15 06:44:47 -0400

    Add lifecycle methods examples

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,12 +1,49 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class Test extends Component {
+  state = {
+    test: 'test'
+  };
+
+  // lifecycle methods examples
+  componentWillMount() {
+    console.log('componentWillMount...');
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount...');
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate...');
+  }
+
+  componentWillUpdate() {
+    console.log('componentWillUpdate...');
+  }
+
+  componentWillReceiveProps(nextProps, nextState) {
+    console.log('componentWillReceiveProps');
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    console.log('getDerivedStateFromProps...');
+    return {
+      test: 'something'
+    };
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('getSnapshotBeforeUpdate...');
+    return null;
+  }
+
   render() {
     return (
       <div>
-        
+        <h1>Test Component</h1>
       </div>
-    )
+    );
   }
 }
 
