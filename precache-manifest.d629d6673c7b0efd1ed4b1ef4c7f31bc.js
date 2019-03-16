e183b10161dccd2ef3d0c60a2e29ca3c554a3d94
Yunier Alvarez <yunieralvarezdev@icloud.com>
2019-03-16 12:37:40 -0400

    Updates

---
+++ precache-manifest.d629d6673c7b0efd1ed4b1ef4c7f31bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "280720acafd57e4cf8d2",
+    "url": "/contact-manager/static/css/main.8eeeaccd.chunk.css"
+  },
+  {
+    "revision": "280720acafd57e4cf8d2",
+    "url": "/contact-manager/static/js/main.048dd16d.chunk.js"
+  },
+  {
+    "revision": "a25dc548b2853493d42e",
+    "url": "/contact-manager/static/js/runtime~main.b61356a3.js"
+  },
+  {
+    "revision": "7dee688d4ca5c03d023c",
+    "url": "/contact-manager/static/css/2.7271b973.chunk.css"
+  },
+  {
+    "revision": "7dee688d4ca5c03d023c",
+    "url": "/contact-manager/static/js/2.2980a9fc.chunk.js"
+  },
+  {
+    "revision": "d0a254e16c454d2d0f5914ed09e26c3d",
+    "url": "/contact-manager/index.html"
+  }
+];
